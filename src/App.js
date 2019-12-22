d90c5e21ae2e91903ccb74e9adc15b8182e8a8a5
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-12-22 23:58:18 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/create-exercise.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
+
 function App() {
   return (
     <Router>
@@ -15,10 +17,8 @@ function App() {
     <Route path="/edit/:id" component={EditExercise} />
     <Route path="create" component={CreateExercise} />
     <Route path="user" component={CreateUser} />
-
     </Router>
   );
 }
 
 export default App;
-1000000002020
